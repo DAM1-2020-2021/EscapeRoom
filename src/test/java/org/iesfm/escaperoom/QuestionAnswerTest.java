0dff9d9105d27d0858861aed0f7c404c5cc7568f
charly <char.sakonia@gamil.com>
2021-03-22 17:15:40 +0100

    test primer metodo hechos

--- src/test/java/org/iesfm/escaperoom/QuestionAnswerTest.java
+++ src/test/java/org/iesfm/escaperoom/QuestionAnswerTest.java
@@ -1,5 +1,6 @@
 package org.iesfm.escaperoom;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class QuestionAnswerTest {
@@ -7,7 +8,36 @@ public class QuestionAnswerTest {
     @Test
     public void correctAnswerTest(){
 
-        QuestionAnswer questionAnswer = new QuestionAnswer();
+        QuestionAnswer questionAnswer = new QuestionAnswer("¿Cuantas patas tiene un perro?", "4");
+
+        boolean res = questionAnswer.checkQuestion("4");
+
+        Assert.assertTrue(res);
 
     }
+    @Test
+    public void incorrectAnswerTest(){
+
+        QuestionAnswer questionAnswer = new QuestionAnswer("¿Cuantas patas tiene un perro?", "4");
+
+        boolean res = questionAnswer.checkQuestion("5");
+
+        Assert.assertFalse(res);
+
+    }
+
+    @Test
+    public void correctAnswerIgnoreCaseTest(){
+
+        QuestionAnswer questionAnswer = new QuestionAnswer("¿Cuantas patas tiene un perro?", "CUATRO");
+
+        boolean res = questionAnswer.checkQuestion("cuatro");
+
+        Assert.assertTrue(res);
+
+    }
+
+
+
+
 }
